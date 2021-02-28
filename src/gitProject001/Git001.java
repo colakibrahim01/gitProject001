41376ed76639dcba75afc6c30e3f4e70d676cc18
colakibrahim01 <63121371+colakibrahim01@users.noreply.github.com>
2021-02-28 11:04:12 -0500

    Update Git001.java

--- src/gitProject001/Git001.java
+++ src/gitProject001/Git001.java
@@ -5,7 +5,7 @@ public class Git001 {
 	public static void main(String[] args) {
 		//Started on Feb 28 2021.
 		int n = 0;
-		//Addition on feb 28
+		//Addition to feb 28
 		String s = "Ibrahim";
 		char c = 'C';
 		
